c1a43131d3ab949aef55fd75d92a42a6345b1911
Alexander Lopez <ClarkAlexander10@gmail.com>
2023-11-11 20:09:40 -0500

    Create ransom-note.ts

---
+++ ransom-note.ts
@@ -0,0 +1,32 @@
+/*
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+Each letter in magazine can only be used once in ransomNote.
+
+Example 1:
+Input: ransomNote = "a", magazine = "b"
+Output: false
+
+Example 2:
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+
+Example 3:
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+*/
+
+function canConstruct(ransomNote: string, magazine: string): boolean {
+    let foundMatches = true;
+    let arrayOfRansomNote = ransomNote.split("");
+    let arrayOfMagazine = magazine.split("");
+
+    arrayOfRansomNote.forEach((item) => {
+        let index = arrayOfMagazine.indexOf(item);
+        if (index == -1){
+            foundMatches = false;
+        }
+        arrayOfMagazine.splice(index, 1);
+    });
+
+    return foundMatches;
+};
