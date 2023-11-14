374c9d7197d4617c98ff17a236791cae32bdf0a2
Alexander Lopez <ClarkAlexander10@gmail.com>
2023-11-14 09:22:02 -0500

    Create single-number.ts

---
+++ single-number.ts
@@ -0,0 +1,40 @@
+/*
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Example 3:
+Input: nums = [1]
+Output: 1
+ 
+Constraints:
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+*/
+
+function singleNumber(nums: number[]): number {
+    let singleNumber = 0;
+    let lengthOfNums = nums.length;
+    let index = 0;
+
+    while (index <= lengthOfNums){
+        let item = nums[index];
+        nums.splice(index, 1);
+        if (nums.indexOf(item) == -1){
+            singleNumber = item;
+            break;
+        } else {
+            nums.push(item)
+        }
+    }
+    
+    return singleNumber;
+};
