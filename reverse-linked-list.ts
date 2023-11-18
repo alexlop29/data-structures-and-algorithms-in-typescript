69f3949f23c0a52857d843d5278bf4f6fe372c01
Alexander Lopez <ClarkAlexander10@gmail.com>
2023-11-18 09:53:12 -0500

    Create reverse-linked-list.ts

---
+++ reverse-linked-list.ts
@@ -0,0 +1,59 @@
+/*
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+ 
+Constraints:
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+ function LinkedListToArray(head: ListNode): number[]{
+    let linkedArray = []
+    while (head != null){
+        linkedArray.push(head.val);
+        head = head.next;
+    }
+    return linkedArray;
+ }
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let arrOfHead = LinkedListToArray(head);
+    arrOfHead.reverse();
+
+    if (arrOfHead.length == 0){
+        return null;
+    }
+
+    let newHead = new ListNode(arrOfHead[0]);
+    let current = newHead;
+
+    for (let i = 1; i < arrOfHead.length; i++) {
+        current.next = new ListNode(arrOfHead[i]);
+        current = current.next;
+    }
+
+    return newHead;
+};
