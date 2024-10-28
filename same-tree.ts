d674a6adc369fd24f8eab84fd9523f87a706c5f2
Alex Lopez <ClarkAlexander10@gmail.com>
2024-10-28 11:52:14 -0400

    Create same-tree.ts

---
+++ same-tree.ts
@@ -0,0 +1,58 @@
+/*
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+Example 1:
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+
+Example 2:
+Input: p = [1,2], q = [1,null,2]
+Output: false
+
+Example 3:
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+
+Constraints:
+The number of nodes in both trees is in the range [0, 100].
+-104 <= Node.val <= 104
+*/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    let value = "";
+
+    function inorderTraversal(node: TreeNode){
+        if (node == null){
+            value = value.concat(String(null));
+            return;
+        };
+        inorderTraversal(node.left);
+        value = value.concat(String(node.val));
+        inorderTraversal(node.right);
+    };
+
+    inorderTraversal(p);
+    let pStr = value;
+
+    value = "";
+
+    inorderTraversal(q);
+    let qStr = value;
+
+    return pStr === qStr;
+};
